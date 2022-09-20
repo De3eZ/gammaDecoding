ac4eed47a5d64da847849fba8903de248867d319
D3e3Z <zhilindanilw@gmail.com>
2022-09-20 18:57:44 +0800

    гамма декодирование

--- src/Client.java
+++ src/Client.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 class Client extends Thread {
     public static void main(String args[]) {
 
-        JFrame frame = new GammaDecription("Pisareva Polina");
+        JFrame frame = new GammaDecription("Zhilin D.E. B-660/var-3.2");
         frame.setVisible(true);
     }
 }
